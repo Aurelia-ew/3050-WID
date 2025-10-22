ae65a7d5a1c1130c175af32767ef05482b9552c5
Aurelia Weickgenannt <aurelia.weickgenannt@students.fhnw.ch>
2025-10-22 12:16:24 +0200

    Teil3

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import { StatelessComponent } from "./StatelessComponent.jsx";
+// import { StatelessComponent } from "./StatelessComponent.jsx";
+import { Header } from "./Header.jsx";
+import { Sidebar } from "./Sidebar.jsx";
+import { Main } from "./MainArea.jsx";
+import { Footer } from "./Footer.jsx";
 
 export function App() {
   // "Named export"
   const [count, setCount] = useState(0);
 
   return (
-    <div>
-      Hallo Welt
-      <StatelessComponent></StatelessComponent>
-      <div> Test </div>
+    <div className="app">
+      <Header></Header>
+      <Sidebar></Sidebar>
+      <Main></Main>
+      <Footer></Footer>
     </div>
   );
 }
-
-// export default App; // "Default export" -> exportieren nur eine Funktion oder Klasse pro Datei
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+//import "./index.css";
 import { App } from "./App.jsx";
 // Wenn ein "default export" verwendet wird, dann muss die Import-Anweisung so aussehen:
 // import {App} from "./App.jsx";
