b40f39bf4428528bb50dfb07a40d7f8c11fbd1e6
Aurelia Weickgenannt <aurelia.weickgenannt@students.fhnw.ch>
2025-10-22 11:34:04 +0200

    teil1
    Test

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { App } from "./App.jsx";
+// Wenn ein "default export" verwendet wird, dann muss die Import-Anweisung so aussehen:
+// import {App} from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
