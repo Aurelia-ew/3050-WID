caf6746682d57bf94eb038b19300b719d6fc7819
Aurelia Weickgenannt <aurelia.weickgenannt@students.fhnw.ch>
2025-10-22 11:46:33 +0200

    Teil2

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from "react";
 import "./App.css";
+import { StatelessComponent } from "./StatelessComponent.jsx";
 
 export function App() {
   // "Named export"
   const [count, setCount] = useState(0);
 
-  return <div>Hallo Welt !!!</div>;
+  return (
+    <div>
+      Hallo Welt
+      <StatelessComponent></StatelessComponent>
+      <div> Test </div>
+    </div>
+  );
 }
 
 // export default App; // "Default export" -> exportieren nur eine Funktion oder Klasse pro Datei
